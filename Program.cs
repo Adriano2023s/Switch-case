3b48a2563f7ed10b10616d5f01434d8a1af10a46
Adriano Sousa <adrianodesousa1998@gmail.com>
2024-05-23 17:38:42 -0300

    Concluído mais uma atividade

--- Program.cs
+++ Program.cs
@@ -6,7 +6,82 @@ namespace Métodos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool loopMenu = true;
+
+            while (loopMenu)
+            {
+                Console.WriteLine("Atividades de Switch case com métodos para revizão.");
+                Console.WriteLine("Escolha uma opção \n");
+                Console.WriteLine("Opção 1: Atividade verificador de números impares e pares.");
+                Console.WriteLine("Opção 2: Atividade coordenadas");
+                Console.WriteLine("opção 3: Atividade intervalos de números.");
+                int menu = int.Parse(Console.ReadLine());
+
+                switch (menu)
+                {
+                    case 1:
+                        ProgramaVerificador();
+                        break;
+                        case 2:
+                        Coordenada();
+                        break;
+                }
+            }
         }
+
+        public static void ProgramaVerificador()
+        {
+            Console.WriteLine("Atividade 1:");
+
+            int numero;
+
+            Console.WriteLine("Digite um número para fazer a verificação.");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine($"O número: {numero}, é par");
+            }
+            else
+            {
+                Console.WriteLine($"O número: {numero}, é impar");
+            }
+        }
+
+public static void Coordenada()
+        {
+            Console.WriteLine("Atividade 2:");
+
+            double x;
+            double y;
+
+            while (true)
+            {
+                Console.WriteLine("Digite o valor de x:");
+                x = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o valor de y:");
+                y = double.Parse(Console.ReadLine());
+
+                if (x > 0 && y > 0)
+                {
+                    Console.WriteLine("Primeiro quadrante");
+                }
+                else if (x > 0 && y < 0)
+                {
+                    Console.WriteLine("Segundo quadrante");
+                }
+                else if (x < 0 && y > 0)
+                {
+                    Console.WriteLine("Terceiro quadrante");
+                }
+                else
+                {
+                    Console.WriteLine("Quarto quadrante");
+                    break;
+                }
+            }
+        }
+
     }
 }
--- obj/Debug/netcoreapp3.1/Métodos.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/Métodos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Métodos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5046c998c12fd29489e449f9c55c7fc090a2f7e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20f6746aa0543e4c54e855498b311492c15ccbbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Métodos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Métodos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
